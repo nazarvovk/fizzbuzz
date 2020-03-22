d42f0ad25e3c73b1dc176ca4c2d31ad22f771310
Nazar Vovk <nazarvovk.work@gmail.com>
2020-03-22 13:18:27 +0200

    я сделяль

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import { useInterval } from './utils';
+import fizz from './fizz.png';
+import fizzAudio from './fizz.ogg';
+import buzz from './buzz.png';
+import buzzAudio from './buzz.ogg';
+import fizzbuzz from './fizzbuzz.png';
+import fizzbuzzAudio from './fizzbuzz.ogg';
+import parrot from './parrot.gif';
+import backgroundMusic from './Darude - Sandstorm.ogg';
 
 function App() {
+  const [timer, setTimer] = useState(1);
+  useInterval(() => setTimer(timer + 1), 600);
+  const shouldRenderFizz = timer % 3 === 0;
+  const shouldRenderBuzz = timer % 5 === 0;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.timer}>{timer}</div>
+      {shouldRenderFizz && shouldRenderBuzz ? (
+        <>
+          <audio
+            autoPlay
+            type='audio/ogg; codecs=vorbis'
+            src={fizzbuzzAudio}
+          ></audio>
+          <img className={styles.fizzbuzz} src={fizzbuzz} alt='buzz' />
+        </>
+      ) : (
+        (shouldRenderFizz && (
+          <>
+            <audio
+              autoPlay
+              type='audio/ogg; codecs=vorbis'
+              src={fizzAudio}
+            ></audio>
+            <img className={styles.fizz} src={fizz} alt='fizz' />
+          </>
+        )) ||
+        (shouldRenderBuzz && (
+          <>
+            <audio
+              autoPlay
+              type='audio/ogg; codecs=vorbis'
+              src={buzzAudio}
+            ></audio>
+            <img className={styles.buzz} src={buzz} alt='buzz' />
+          </>
+        ))
+      )}
+      <img className={styles.parrot} src={parrot} alt='' />
+      <audio
+        autoPlay
+        loop
+        type='audio/ogg; codecs=vorbis'
+        src={backgroundMusic}
+      ></audio>
     </div>
   );
 }

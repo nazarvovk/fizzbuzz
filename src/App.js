9f3feef43ee3d66cd31dab269bef71b1b6406fa9
Nazar Vovk <nazarvovk.work@gmail.com>
2020-03-23 17:44:08 +0200

    p e r f o r m a n c e

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
 import { useInterval } from './utils';
-import fizz from './fizz.png';
-import fizzAudio from './fizz.ogg';
-import buzz from './buzz.png';
-import buzzAudio from './buzz.ogg';
-import fizzbuzz from './fizzbuzz.png';
-import fizzbuzzAudio from './fizzbuzz.ogg';
 import parrot from './parrot.gif';
+import fizzbuzz from './fizzbuzz.png';
+import fizz from './fizz.png';
+import buzz from './buzz.png';
 import backgroundMusic from './Darude - Sandstorm.ogg';
+import fizzbuzzSrc from './fizzbuzz.ogg';
+import fizzSrc from './fizz.ogg';
+import buzzSrc from './buzz.ogg';
+
+const fizzAudio = new Audio(fizzSrc);
+const buzzAudio = new Audio(buzzSrc);
+const fizzbuzzAudio = new Audio(fizzbuzzSrc);
 
 function App() {
   const [timer, setTimer] = useState(1);
-  useInterval(() => setTimer(timer + 1), 600);
+  useInterval(() => setTimer(timer + 1), 800);
   const shouldRenderFizz = timer % 3 === 0;
   const shouldRenderBuzz = timer % 5 === 0;
+  if (shouldRenderFizz && shouldRenderBuzz) {
+    fizzbuzzAudio.play();
+  } else if (shouldRenderFizz) {
+    fizzAudio.play();
+  } else if (shouldRenderBuzz) {
+    buzzAudio.play();
+  }
   return (
     <div className={styles.app}>
       <div className={styles.timer}>{timer}</div>
-      {shouldRenderFizz && shouldRenderBuzz ? (
-        <>
-          <audio
-            autoPlay
-            type='audio/ogg; codecs=vorbis'
-            src={fizzbuzzAudio}
-          ></audio>
-          <img className={styles.fizzbuzz} src={fizzbuzz} alt='buzz' />
-        </>
-      ) : (
-        (shouldRenderFizz && (
-          <>
-            <audio
-              autoPlay
-              type='audio/ogg; codecs=vorbis'
-              src={fizzAudio}
-            ></audio>
-            <img className={styles.fizz} src={fizz} alt='fizz' />
-          </>
-        )) ||
-        (shouldRenderBuzz && (
-          <>
-            <audio
-              autoPlay
-              type='audio/ogg; codecs=vorbis'
-              src={buzzAudio}
-            ></audio>
-            <img className={styles.buzz} src={buzz} alt='buzz' />
-          </>
-        ))
-      )}
+      <img
+        className={styles.fizzbuzz}
+        style={{
+          display: shouldRenderFizz && shouldRenderBuzz ? 'block' : 'none',
+        }}
+        src={fizzbuzz}
+        alt='buzz'
+      />
+      <img
+        className={styles.fizz}
+        style={{
+          display: shouldRenderFizz && !shouldRenderBuzz ? 'block' : 'none',
+        }}
+        src={fizz}
+        alt='fizz'
+      />
+      <img
+        className={styles.buzz}
+        style={{
+          display: shouldRenderBuzz && !shouldRenderFizz ? 'block' : 'none',
+        }}
+        src={buzz}
+        alt='buzz'
+      />
       <img className={styles.parrot} src={parrot} alt='' />
       <audio
         autoPlay
